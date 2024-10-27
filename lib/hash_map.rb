d2ed9a0f61bff643321109858db24b007f79b796
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-27 19:24:54 +0100

    Add public interface methods and class description

---
+++ lib/hash_map.rb
@@ -0,0 +1,34 @@
+# Hash map data structure implementation
+class HashMap
+  attr_accessor :capacity, :load_factor
+
+  def hash(string)
+  end
+
+  def set(key, value)
+  end
+
+  def get(key)
+  end
+
+  def has?(key)
+  end
+
+  def remove(key)
+  end
+
+  def length
+  end
+
+  def clear
+  end
+
+  def keys
+  end
+
+  def values
+  end
+
+  def entries
+  end
+end
