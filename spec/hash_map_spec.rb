04aed7a4156099755df89ba83ae6c73868b1c539
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-27 17:42:27 +0100

    Add tests for hashing function

--- spec/hash_map_spec.rb
+++ spec/hash_map_spec.rb
@@ -3,6 +3,17 @@ require_relative "lib/hash_map"
 
 RSpec.describe HashMap do
   describe "#hash" do
+    it "returns an integer when given a string" do
+      expect(subject.hash("abc")).to be_a Integer
+    end
+
+    it "returns different integers for different string inputs" do
+      expect(subject.hash("abc")).to_not eql subject.hash("def")
+    end
+
+    it "returns different integers for string that differ only in letter order" do
+      expect(subject.hash("abc")).to_not eql subject.hash("cba")
+    end
   end
 
   describe "#set" do
