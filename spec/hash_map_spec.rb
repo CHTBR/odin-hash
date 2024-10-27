1ccc93d90ad34768753b1371924ff3bd62642cbb
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-27 17:33:54 +0100

    Add public interface descriptions

---
+++ spec/hash_map_spec.rb
@@ -0,0 +1,34 @@
+require "rspec"
+require_relative "lib/hash_map"
+
+RSpec.describe HashMap do
+  describe "#hash" do
+  end
+
+  describe "#set" do
+  end
+
+  describe "#get" do
+  end
+
+  describe "#has?" do
+  end
+
+  describe "#remove" do
+  end
+
+  describe "#length" do
+  end
+
+  describe "#clear" do
+  end
+
+  describe "#keys" do
+  end
+
+  describe "#values" do
+  end
+
+  describe "#entries" do
+  end
+end
