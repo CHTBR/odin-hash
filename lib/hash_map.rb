65df2c4d56ef38709e36ef703ff388496eaca445
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-30 09:04:07 +0100

    Add #entries, init and basic #set Set only works for unique hash codes

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,6 +1,14 @@
+require_relative "linked_list/linked_list"
+
 # Hash map data structure implementation
 class HashMap
   attr_accessor :capacity, :load_factor
+
+  def initialize
+    @capacity = 16
+    @load_factor = 0.8
+    @buckets = Array.new(@capacity) { LinkedList.new }
+  end
 
   def hash(string)
     hash = 0
@@ -10,6 +18,22 @@ class HashMap
   end
 
   def set(key, value)
+    hash_code = hash(key)
+    bucket_index = hash_code % capacity
+    bucket = @buckets[bucket_index]
+    list_index = nil
+    if bucket.any? do |arr, index|
+      if arr[0] == key
+        list_index = index
+        return true
+      end
+      false
+    end
+      bucket.remove_at(list_index)
+      bucket.insert_at(list_index, [key, value])
+    else
+      bucket.append([key, value])
+    end
   end
 
   def get(key)
@@ -34,5 +58,10 @@ class HashMap
   end
 
   def entries
+    entries = []
+    @buckets.each do |bucket|
+      bucket.entries.each { |entry| entries.append entry }
+    end
+    entries
   end
 end
